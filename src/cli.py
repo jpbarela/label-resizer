639364031ef2c9b393db69dd64fd9734dafcad05
JP Barela <barela@jpbarela.com>
2026-01-21 07:21:18 -0700

    Add passing a starting position from the CLI (#2)
    Adds a command line option to specify the starting position of the
    labels on the first page

--- src/cli.py
+++ src/cli.py
@@ -5,11 +5,13 @@ from pdf_manipulator import transform_pdf
 @click.command()
 @click.argument("input_file", type=str, metavar='<input_file>')
 @click.argument("output_file", type=str, metavar='<output_file>')
-def label_resize(input_file: str, output_file: str) -> None:
+@click.option('--position', help='the starting position of the labels', type=int, default=1)
+def label_resize(input_file: str, output_file: str, position: int = 1) -> None:
     """
     Transforms <input_file> into the <output_file>
     """
-    transform_pdf(input_file, output_file)
+    # internally use zero-based indexing but start at 1 for the cli
+    transform_pdf(input_file, output_file, position=position-1)
 
 
 if __name__ == '__main__':
